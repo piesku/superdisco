6c24daf172cf46596576aaa1f6d7687d024a57f8
Staś Małolepszy <stas@piesku.com>
2019-03-31 23:00:45 +0200

    uniform3f

--- index.js
+++ index.js
@@ -20,12 +20,12 @@ let o, a, e, t, n = Float32Array.of(
     c = s = .6,
     f = g.cr(),
     d = o => {
-        g.c(16640), g.u3(e, Float32Array.of(o, c, s)), g.dat(5, 0, 14, 499849), requestAnimationFrame(d)
+        g.c(16640), g.uniform3f(e, o, c, s), g.dat(5, 0, 14, 499849), requestAnimationFrame(d)
     };
 
 g.ah(f, r(35633,
     `#version 300 es
-uniform ivec3 n;in vec3 p;out vec4 f;const mat4 v=mat4(1.299,0.,0.,0.,0.,1.732,0.,0.,0.,0.,-1.002,-1.,0.,0.,-2.002,0.);vec3 s(float v,float s){float m=-707.+mod(v,707.)*2.,c=-707.+v/707.*2.,g=c+floor(s/2.)*2.;return vec3(m,floor(9.*sin(m/30.)*sin(g/20.)+99.*sin(m/99.)*sin(g/299.)+4.*fract(sin(g)*99.)),c-mod(s,2.));}void main(){float m=float(n.x)/99.,c=9.*sin(-m/30.)+30.,g=float(n.y)*.0063,D=float(n.z)*.0016-.8;f=v*mat4(cos(g),sin(g)*sin(D),-sin(g)*cos(D),0.,0.,cos(D),sin(D),0.,sin(g),-cos(g)*sin(D),cos(g)*cos(D),0.,0.,-c,0.,1.)*vec4(p+s(float(gl_InstanceID),m),1.);gl_Position=f;}`
+uniform vec3 n;in vec3 p;out vec4 f;const mat4 v=mat4(1.299,0.,0.,0.,0.,1.732,0.,0.,0.,0.,-1.002,-1.,0.,0.,-2.002,0.);vec3 s(float v,float s){float m=-707.+mod(v,707.)*2.,c=-707.+v/707.*2.,g=c+floor(s/2.)*2.;return vec3(m,floor(9.*sin(m/30.)*sin(g/20.)+99.*sin(m/99.)*sin(g/299.)+4.*fract(sin(g)*99.)),c-mod(s,2.));}void main(){float m=n.x/99.,c=9.*sin(-m/30.)+30.,g=n.y*.0063,D=n.z*.0016-.8;f=v*mat4(cos(g),sin(g)*sin(D),-sin(g)*cos(D),0.,0.,cos(D),sin(D),0.,sin(g),-cos(g)*sin(D),cos(g)*cos(D),0.,0.,-c,0.,1.)*vec4(p+s(float(gl_InstanceID),m),1.);gl_Position=f;}`
 )), g.ah(f, r(35632,
     `#version 300 es
     precision lowp float;in vec4 f;out vec4 c;void main(){c=mix(vec4(normalize(cross(dFdx(f).xyz,dFdy(f).xyz)),1.),vec4(1.,.7,0.,1.),clamp(length(f-vec4(0.))/999.,0.,1.));}`

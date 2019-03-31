cccd21e1d3363d2df67dc738f5e9f39bc0978bee
Michał Budzyński <michal@virtualdesign.pl>
2019-04-01 00:28:34 +0200

    1017b

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-for(_='in~s~(Z9.*ZYatXmX4W=n.V707UvecT]+[i[R TQ,0OmoM.)LL*Zg/2K(fJJ).xyzfloor(=(o=>{Child(1,-9,T4((34962,=-U.+uniform999,),Don;outQ4 g)*g.c.,0			g.;}"cS(oo)void ma~(){cos(floXahJ,(o=h(3563so(o,"#versi 300 es\\nfor(i ~ g=b.appendb.reMvea).cleNode()).getCtext("webgl2"g)g[i[0R7]R13]]]=g[i];l(.7O,1e(2929n=FloX32Array.of(------9,-1c=s=.6,f=r(d(166403f(e,o,c,sdX(5O,14,5e5requestAnimXiFrame(d)}3Q3 n;~Q3 pf;W v=W(1.3,2-1-1-20L;T3 s( v, s){ mMd(v,UL*2cv/U.*2g=c+s/2L*2.;returnQ3(m,Ym/30K0L+.*Zm/KL+4.*fract(ZLc-Md(s,2L);} mVx/c=Y-m/30L+3	gVy*.006,DVz*.002-.8;f=v*W(gZZ-ZZ	Zg-Z-c,	1L*p+s((gl_InstanceIm1L;gl_Positi=f2precisi lowp ;~Q4 fc;c=mix(normalize(cross(dFdx,dFdy)1.1.7,	1.clamp(lengthJ-0L)/9	1L)lgJe=g.goaJ,"n"urJbfu()ban,35044eet(0vto(0,3,5126OOOb.MuseMvec=o.x,s=o.y}d(0);';G=/[^ -INPS[-}]/.exec(_);)with(_.split(G))_=join(shift());eval(_)
+g = b.appendChild(b.removeChild(a).cloneNode()).getContext("webgl2");
+for (i in g) g[i[0] + [i[7]] + [i[13]]] = g[i];
+g.cl(1, .7, 0, 1), g.e(2929);
+n = Float32Array.of(
+    -1, 9, 1,
+    1, 9, 1,
+    -1, -9, 1,
+    1, -9, 1,
+    1, -9, -1,
+    1, 9, 1,
+    1, 9, -1,
+    -1, 9, 1,
+    -1, 9, -1,
+    -1, -9, 1,
+    -1, -9, -1,
+    1, -9, -1,
+    -1, 9, -1,
+    1, 9, -1
+),
+    c = s = .6,
+    f = g.cr(),
+    d = o => {
+        g.c(16640), g.uniform3f(e, o, c, s), g.dat(5, 0, 14, 5e5), requestAnimationFrame(d)
+    };
+
+g.ah(f, (o = g.ch(35633), g.so(o,
+    `#version 300 es
+uniform vec3 n;in vec3 p;out vec4 f;mat4 v=mat4(1.3,0.,0.,0.,0.,2.,0.,0.,0.,0.,-1.,-1.,0.,0.,-2.,0.);vec3 s(float v,float s){float m=-707.+mod(v,707.)*2.,c=-707.+v/707.*2.,g=c+floor(s/2.)*2.;return vec3(m,floor(9.*sin(m/30.)*sin(g/20.)+99.*sin(m/99.)*sin(g/299.)+4.*fract(sin(g)*99.)),c-mod(s,2.));}void main(){float m=n.x/99.,c=9.*sin(-m/30.)+30.,g=n.y*.006,D=n.z*.002-.8;f=v*mat4(cos(g),sin(g)*sin(D),-sin(g)*cos(D),0.,0.,cos(D),sin(D),0.,sin(g),-cos(g)*sin(D),cos(g)*cos(D),0.,0.,-c,0.,1.)*vec4(p+s(float(gl_InstanceID),m),1.);gl_Position=f;}`
+), g.cS(o), o)), g.ah(f, (o = g.ch(35632), g.so(o,
+    `#version 300 es
+    precision lowp float;in vec4 f;out vec4 c;void main(){c=mix(vec4(normalize(cross(dFdx(f).xyz,dFdy(f).xyz)),1.),vec4(1.,.7,0.,1.),clamp(length(f-vec4(0.))/999.,0.,1.));}`
+), g.cS(o), o)), g.lg(f), e = g.goa(f, "n"), g.ur(f), g.bf(34962, g.cu()), g.ba(34962, n, 35044), g.eet(0), g.vto(0, 3, 5126, 0, 0, 0), b.onmousemove = o => {
+    c = o.x, s = o.y
+}, d(0);

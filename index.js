73e356dcc3ad3ebc6f0989c6b883b172e47c3f4c
Michał Budzyński <michal@virtualdesign.pl>
2019-03-31 21:45:02 +0200

    regpacked index

--- index.js
+++ index.js
@@ -1,151 +1,18 @@
-g.clearColor(1, .7, 0, 1);
-g.enable(g.DEPTH_TEST);
-g.enable(g.CULL_FACE);
-
-// The number of voxels along the edge of the world.
-let EDGE_COUNT = 707,
-    shader,
-    vertices = Float32Array.of(
-        1, 9, 1, -1, 9, 1, -1, -9, 1,
-        1, -9, 1, 1, 9, 1, -1, -9, 1,
-        1, 9, 1, 1, -9, 1, 1, -9, -1,
-        1, 9, -1, 1, 9, 1, 1, -9, -1,
-        1, 9, 1, 1, 9, -1, -1, 9, -1,
-        -1, 9, 1, 1, 9, 1, -1, 9, -1,
-        -1, -9, -1, -1, 9, -1, 1, 9, -1,
-        1, -9, -1, -1, -9, -1, 1, 9, -1,
-        -1, -9, -1, -1, -9, 1, -1, 9, 1,
-        -1, 9, -1, -1, -9, -1, -1, 9, 1,
-        -1, -9, -1, 1, -9, -1, 1, -9, 1,
-        -1, -9, 1, -1, -9, -1, 1, -9, 1
-    ),
-    compile_shader = (type, source) => {
-        shader = g.createShader(type);
-        g.shaderSource(shader, source);
-        g.compileShader(shader);
-
-        if (!g.getShaderParameter(shader, g.COMPILE_STATUS)) // DEBUG
-            throw new Error(g.getShaderInfoLog(shader)); // DEBUG
-
-        return shader;
-    },
-    mousex = 0.6,
-    mousey = 0.6,
-    audio,
-    program = g.createProgram(),
-    uniform_now, uniform_mouse,
-    timestamp = 0,
-    processor,
-    tick = now => {
-        timestamp = now;
-        g.clear(16640);
-        g.uniform1f(uniform_now, now);
-        g.uniform2f(uniform_mouse, mousex, mousey);
-        g.drawArraysInstanced(g.TRIANGLES, 0, 36, EDGE_COUNT * EDGE_COUNT);
-
-        requestAnimationFrame(tick);
+for (i in g) g[i[0] + [i[7]] + [i[13]]] = g[i];
+g.cl(1, .7, 0, 1), g.e(2929), g.e(2884);
+let o, e, t, n, r, c = Float32Array.of(1, 9, 1, -1, 9, 1, -1, -9, 1, 1, -9, 1, 1, 9, 1, -1, -9, 1, 1, 9, 1, 1, -9, 1, 1, -9, -1, 1, 9, -1, 1, 9, 1, 1, -9, -1, 1, 9, 1, 1, 9, -1, -1, 9, -1, -1, 9, 1, 1, 9, 1, -1, 9, -1, -1, -9, -1, -1, 9, -1, 1, 9, -1, 1, -9, -1, -1, -9, -1, 1, 9, -1, -1, -9, -1, -1, -9, 1, -1, 9, 1, -1, 9, -1, -1, -9, -1, -1, 9, 1, -1, -9, -1, 1, -9, -1, 1, -9, 1, -1, -9, 1, -1, -9, -1, 1, -9, 1),
+    f = (e, a) => (o = g.ch(e), g.so(o, a), g.cS(o), o),
+    s = .6,
+    l = .6,
+    m = g.cr(),
+    d = 0,
+    A = o => {
+        d = o, g.clear(16640), g.uniform1f(t, o), g.uniform2f(n, s, l), g.dat(4, 0, 36, 499849), requestAnimationFrame(A)
     };
-
-g.attachShader(program, compile_shader(g.VERTEX_SHADER, `#version 300 es
-    uniform float n;
-    uniform vec2 m;
-    // Vertex position in the mesh
-    in vec3 p;
-    // Vertex position in the instance
-    out vec4 f;
-
-    // Projection matrix
-    const mat4 P=mat4(
-        1.299,0.,0.,0.,
-        0.,1.732,0.,0.,
-        0.,0.,-1.002,-1.,
-        0.,0.,-2.002,0.);
-
-    // Compute the translation of the instance
-    vec3 t(float id,float o){
-        float x=-${EDGE_COUNT}.+mod(id,${EDGE_COUNT}.)*2.;
-        float z=-${EDGE_COUNT}.+(id/${EDGE_COUNT}.)*2.;
-        // Make offset discrete in increments of the cube's width.
-        float Z=z+floor(o/2.)*2.;
-        return vec3(
-            x,
-            floor(
-                // y
-                9.*sin(x/30.)*sin(Z/20.)
-                // Hills and valleys
-                +99.*sin(x/99.)*sin(Z/299.)
-                // Random noise, constant for a given (x, Z)
-                +4.*fract(sin(Z)*99.)),
-            z-mod(o,2.));
-    }
-
-    void main(){
-        // The offset of the world
-        float o=n/99.;
-        // The position of the camera
-        float y=9.*sin(-o/30.)+30.;
-
-        // Yaw
-        float a=m.x*6.3;
-        // Pitch
-        float b=m.y*1.6-.8;
-
-        f = P * mat4(
-            cos(a),sin(a)*sin(b),-sin(a)*cos(b),0.,
-            0.,cos(b),sin(b),0.,
-            sin(a),-cos(a)*sin(b),cos(a)*cos(b),0.,
-            0.,-y,0.,1.)*vec4(p+t(float(gl_InstanceID),o),1.);
-        gl_Position=f;
-    }
-`));
-g.attachShader(program, compile_shader(g.FRAGMENT_SHADER, `#version 300 es
-    precision lowp float;
-
-    // Fragment position
-    in vec4 f;
-    // Fragment color
-    out vec4 c;
-
-    void main(){
-        c=mix(
-            // Normal of the fragment
-            vec4(normalize(cross(dFdx(f).xyz,dFdy(f).xyz)),1.),
-            // Fog color
-            vec4(1.,.7,0.,1.),
-            // Divide length by max fog distance
-            clamp(length(f-vec4(0.))/999.,0.,1.));
-    }
-`));
-g.linkProgram(program);
-
-uniform_now = g.getUniformLocation(program, "n");
-uniform_mouse = g.getUniformLocation(program, "m");
-
-
-// And make it the only active one.
-g.useProgram(program);
-// Buffer vertex data for a cube.
-g.bindBuffer(g.ARRAY_BUFFER, g.createBuffer());
-g.bufferData(g.ARRAY_BUFFER, vertices, g.STATIC_DRAW);
-g.enableVertexAttribArray(0);
-g.vertexAttribPointer(0, 3, g.FLOAT, g.FALSE, 0, 0);
-b.onmousemove = e => {
-    mousex = e.x / a.width;
-    mousey = e.y / a.height;
-};
-
-b.onclick = e => {
-    if (!audio) {
-        audio = new AudioContext();
-        processor = audio.createScriptProcessor(2048);
-        processor.onaudioprocess = o => {
-            for (i = 2048; i--;) {
-                o.outputBuffer.getChannelData(0)[i] =
-                    Math.sin(timestamp / i) / 99 * mousey;
-            }
-        };
-        processor.connect(audio.destination);
-    }
-};
-
-tick(0);
+g.ah(m, f(35633, "#version 300 es\nuniform float n;uniform vec2 m;in vec3 p;out vec4 f;const mat4 P=mat4(1.299,0.,0.,0.,0.,1.732,0.,0.,0.,0.,-1.002,-1.,0.,0.,-2.002,0.);vec3 t(float id,float o){float x=-707.+mod(id,707.)*2.;float z=-707.+(id/707.)*2.;float Z=z+floor(o/2.)*2.;return vec3(x,floor(9.*sin(x/30.)*sin(Z/20.)+99.*sin(x/99.)*sin(Z/299.)+4.*fract(sin(Z)*99.)),z-mod(o,2.));}void main(){float o=n/99.;float y=9.*sin(-o/30.)+30.;float a=m.x*6.3;float b=m.y*1.6-.8;f = P * mat4(cos(a),sin(a)*sin(b),-sin(a)*cos(b),0.,0.,cos(b),sin(b),0.,sin(a),-cos(a)*sin(b),cos(a)*cos(b),0.,0.,-y,0.,1.)*vec4(p+t(float(gl_InstanceID),o),1.);gl_Position=f;}")), g.ah(m, f(35632, "#version 300 es\nprecision lowp float;in vec4 f;out vec4 c;void main(){c=mix(vec4(normalize(cross(dFdx(f).xyz,dFdy(f).xyz)),1.),vec4(1.,.7,0.,1.),clamp(length(f-vec4(0.))/999.,0.,1.));}")), g.lg(m), t = g.goa(m, "n"), n = g.goa(m, "m"), g.ur(m), g.bf(34962, g.cu()), g.ba(34962, c, 35044), g.eet(0), g.vto(0, 3, 5126, g.FALSE, 0, 0), b.onmousemove = (o => {
+    s = o.x / a.width, l = o.y / a.height
+}), b.onclick = (o => {
+    e || (e = new AudioContext, (r = e.createScriptProcessor(2048)).onaudioprocess = (o => {
+        for (i = 2048; i--;) o.outputBuffer.getChannelData(0)[i] = Math.sin(d / i) / 99 * l
+    }), r.connect(e.destination))
+}), A(0);
